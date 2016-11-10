6965a0e075479e14249eb8d2c854054cab1bba70
zhaochunliang <zhaochunliang@ethercap.com>
2016-11-10 16:48:07 +0800

    add file

---
+++ tools/git_util2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# git clone xxx.git
+### change file
+# git add .
+# git commit -m 'some chage'
+# cd xxx/.git
+#######change config http://github.com/xx->http://zhaochl@github.com/xxx
+# git push origin master:zcl
+
+#git delete
+#git push origin :zcl
